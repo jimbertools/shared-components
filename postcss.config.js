6a0ab98818dd2ae3efd495b066526eea2115d15d
tobiaschielens <tobias.chielens@gmail.org>
2021-07-08 22:35:19 +0200

    getting tailwind in using postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('tailwindcss')('./tailwind.config.js'),
+    require('autoprefixer'),
+    //@todo check how to include elementplus theme in build
+    // require('element-plus/lib/theme-chalk/index.css')
+],
 }
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue';
 import App from './App.vue'
 import './index.css';
 
+import 'element-plus/lib/theme-chalk/index.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
 const app = createApp(App)
 app.mount('#app')
