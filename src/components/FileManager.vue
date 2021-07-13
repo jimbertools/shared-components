80c219ced2aa8dc821eb7f1fcae9fb784eec6254
Rémi Teissier <rteissier.code@chillpc.fr>
2021-07-13 15:27:14 +0200

    Good good emit

--- src/components/FileManager.vue
+++ src/components/FileManager.vue
@@ -65,7 +65,7 @@
                   :defaultSort="sort"
                   @[TableEmits.OpenItem]="data => $emit(Emits.OpenItem, data)"
                   @[TableEmits.SortChanged]="e => $emit(Emits.SortChanged, e)"
-                  @[TableEmits.SelectedItems]="e => $emit(Emits.SelectedItems, e)"
+                  @[TableEmits.SelectedChanged]="e => $emit(Emits.SelectedChanged, e)"
                 >
                   <template v-if="!hasSlot('data-name')" #data-name="rowData">
                     <em :class="getIcon(rowData.row.fileType) + ' ' + getIconColor(rowData.row.fileType)"></em>
