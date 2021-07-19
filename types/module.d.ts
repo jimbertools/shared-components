f3e8edd2958325e2bdc53808b7d715b9403a984f
JensMeire <jens.meire>
2021-07-19 02:08:23 +0200

    MinWidth

--- types/module.d.ts
+++ types/module.d.ts
@@ -1,4 +1,8 @@
-import { ComponentPublicInstance } from 'vue';
+import {ComponentPublicInstance} from "vue"
+declare module "@jimber/shared-components"
 
-declare module '@jimber/shared-components';
-export * from '/index.ts';
+
+export const Button : ComponentPublicInstance
+export const TestTable : ComponentPublicInstance
+export const GridView : ComponentPublicInstance
+export const FileManager : ComponentPublicInstance
