68df0f1edf5cb5ab5205c6b1a2e5ac49dd65c30b
Dean Verjans <developer.dean@outlook.com>
2022-03-16 12:41:43 +0100

    Add relative to td in table

--- src/components/Table/Table.vue
+++ src/components/Table/Table.vue
@@ -57,7 +57,7 @@
                     @dragend="dragEnd"
                 >
                     <td
-                        class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
+                        class="relative px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
                         v-for="header in headers"
                         :data-name="`data-${header.key}`"
                         :key="data[header.key]"
